caffe99fb5f24513dcd63d0620d8ab95ee773b1b
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2024-01-20 01:52:34 -0800

    map + node modules

--- recycle-this/App.tsx
+++ recycle-this/App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <HomeScreen />;
 }
+
 
 const styles = StyleSheet.create({
   container: {
